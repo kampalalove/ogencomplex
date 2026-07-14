3fd5fe9ebd33ff1485db5a14402fea14e34df485
Mark <mark@example.com>
2026-07-14 03:51:21 -0400

    feat: your change description

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const http = require('http'); 
-const server = http.createServer((req, res) =
-  res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-  res.end('ogencomplex is running!\n'); 
-}); 
-server.listen(8080, () =
-  console.log('Server running on port 8080'); 
-}); 
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('ogencomplex is running!\n');
+});
+server.listen(process.env.PORT || 8080, () => {
+  console.log('Server running on port', process.env.PORT || 8080);
+});
